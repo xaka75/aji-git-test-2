cb3ac79bfa44370282dac732a0721e1915074ebb
xaka75 <sakastrz123@gmail.com>
2024-11-11 10:07:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, dignissimos et corporis tempore ratione, qui placeat provident ab nobis voluptatem dolor aut quia nulla aperiam blanditiis possimus deleniti voluptatum asperiores!</p>
+  <ul>
+    <li>Satu</li>
+    <li>Dua</li>
+  </ul>
 </body>
 </html>
