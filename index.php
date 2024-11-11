43b6b75cb006cb486cdc89e015d1dc74c5aa342c
xaka75 <sakastrz123@gmail.com>
2024-11-11 09:39:50 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
   <h1>Hello World!</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, dignissimos et corporis tempore ratione, qui placeat provident ab nobis voluptatem dolor aut quia nulla aperiam blanditiis possimus deleniti voluptatum asperiores!</p>
 </body>
 </html>
